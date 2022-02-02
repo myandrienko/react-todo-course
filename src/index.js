6e3ed7b518ddd549a82306ed50a60cce4444da9d
Matvey Andrienko <m.y.andrienko@outlook.com>
2022-02-02 18:41:12 +0300

    add todo list and item

--- src/index.js
+++ src/index.js
@@ -1,6 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-ReactDOM.render('Hello, world',
+const todos = [
+  { name: 'First task', done: true, urgent: false },
+  { name: 'Second task', done: false, urgent: true }
+];
+
+function TodoItem({ name, done, urgent }) {
+  return <label><input type="checkbox" checked={done}/>{name}</label>
+}
+
+function TodoList({ list }) {
+  const elements = list.map(item => <TodoItem {...item} />);
+  return <TodoItem name={list[0].name} done={list[0].done}/>
+}
+
+const element = <TodoList list={todos}/>;
+console.log(element);
+
+ReactDOM.render(element,
   document.getElementById('root')
 );
